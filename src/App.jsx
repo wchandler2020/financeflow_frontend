68cfec4c8792a9559259b7a5cc7666b6c9d0398d
wchandler2020 <william.d.chandler1@gmail.com>
2026-02-16 17:24:07 -0600

    added: deployment to github-pages configureation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './context/AuthContext';
 import { useAuth } from './hooks/useAuth';
